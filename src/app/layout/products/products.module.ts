e55553e8b7537f3993e71a87f7aea9a93019df09
Bartosz <bartoszmrosek@op.pl>
2023-06-05 17:03:14 +0200

    Styling matching figma design

--- src/app/layout/products/products.module.ts
+++ src/app/layout/products/products.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { FeaturedImageComponent } from './components/featured-image/featured-image.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    FeaturedImageComponent
+    FeaturedImageComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule
