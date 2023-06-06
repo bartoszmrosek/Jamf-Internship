6508c305bb7e5833d0af3c04c1866928697967ab
Bartosz <bartoszmrosek@op.pl>
2023-06-06 20:34:55 +0200

    Added github workflow for testing

--- src/app/shared/styled-btn/styled-btn.component.spec.ts
+++ src/app/shared/styled-btn/styled-btn.component.spec.ts
@@ -3,9 +3,13 @@ import { fireEvent, render, screen } from '@testing-library/angular';
 import { StyledBtnComponent } from './styled-btn.component';
 
 describe('StyledBtnComponent', () => {
+  const BTN_TYPE = "submit";
   const clickEvent = jest.fn()
   beforeEach(async ()=>{
     await render(StyledBtnComponent, {
+      componentInputs: {
+        btnType: BTN_TYPE
+      },
       componentOutputs:{
         clickEvent: {
           emit: clickEvent
@@ -16,5 +20,8 @@ describe('StyledBtnComponent', () => {
   it("should fire emit on click event", ()=>{
     fireEvent.click(screen.getByRole("button"));
     expect(clickEvent).toHaveBeenCalledTimes(1)
+  })
+  it("should set proper type based on input", ()=>{
+    expect(screen.getByRole("button")).toHaveProperty("type", BTN_TYPE)
   })
 });
