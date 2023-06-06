c5d4519ca298e2643da9180fa9aa415a1c39dc1c
Bartosz <bartoszmrosek@op.pl>
2023-06-06 21:10:50 +0200

    Changed semantics and styling

--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -1,10 +1,21 @@
-import {render, screen, within} from "@testing-library/angular"
+import {fireEvent, render, screen, within} from "@testing-library/angular"
 
 import { NavbarComponent } from './navbar.component';
+import { ProductsService } from "../../../app/services/products/products.service";
 
 describe('NavbarComponent', () => {
+  const formOpeningMock = jest.fn();
   beforeEach(async ()=>{
-    await render(NavbarComponent)
+    await render(NavbarComponent, {
+      componentProviders: [
+        {
+          provide: ProductsService,
+          useValue: {
+            openAddingForm: formOpeningMock
+          }
+        }
+      ]
+    })
   })
   it("should contain jamf logo", async ()=>{
     expect(screen.getByAltText("Logo Jamf")).toBeInTheDocument()
@@ -12,5 +23,9 @@ describe('NavbarComponent', () => {
   it("should contain button to add new form", ()=>{
     const button = screen.getByRole("button");
     expect(within(button).getByAltText("Ikona plusa")).toBeInTheDocument()
+  })
+  it("should fire form opening callback on click", ()=>{
+    fireEvent.click(screen.getByRole("button"));
+    expect(formOpeningMock).toHaveBeenCalledTimes(1)
   })
 });
